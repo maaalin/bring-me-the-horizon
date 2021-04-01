41d20e4d00e9876bd842d0d0f8e5e606604e7569
maaalin <78012771+maaalin@users.noreply.github.com>
2021-04-01 17:15:02 +0200

    //

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 // typewriter effect
 var i = 0;
-var txt = 'nah... sorry all the concerts are fake'; /* The text */
-var speed = 100; /* The speed/duration of the effect in milliseconds */
+var txt = 'nah... sorry all the concerts are fake'; 
+var speed = 100;
 
 function typeWriter() {
   if (i < txt.length) {
@@ -15,7 +15,7 @@ function typeWriter() {
 
 // blue mode
 if (typeof (localStorage) == 'undefined') {
-    document.getElementById("result").innerHTML = 
+    document.getElementById("result").innerHTML =
 'Your browser does not support HTML5 localStorage. Try upgrading.';
 } else {
     if (localStorage.getItem("fav") != null) {
@@ -26,15 +26,13 @@ if (typeof (localStorage) == 'undefined') {
 $(document).ready(function () {
     $('.blueMode').on('click', function () {
         getFav = localStorage.fav;
-        //$(".item").removeClass('favorites');
-        //localStorage.removeItem('background');
         $(this).closest(".all").toggleClass('lightMode');
 		if ($(this).closest(".all").hasClass('lightMode')) {
         localStorage.setItem('fav', 'lightMode');
-        
+
     }else{
 		localStorage.removeItem('fav');
 	}
-        
+
     });
 });
